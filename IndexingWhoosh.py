5b7bf420ccaa4587e34cedd2116a3dc317df9d16
241253 <241253@studenti.unimore.it>
2020-04-25 17:35:59 +0200

    J: IndexingWhoosh - SearchingWhoosh

--- IndexingWhoosh.py
+++ IndexingWhoosh.py
@@ -1,33 +1,38 @@
+import os
 from whoosh.fields import *
-from xmlparserSAX import pagina, getParsedPage
+from whoosh.index import create_in
+import xmlparserSAX
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+
 
 def removeStopwordsLemmatizeStemming(tokens):
     wnl = nltk.WordNetLemmatizer()
     porter = PorterStemmer()
     return [porter.stem(wnl.lemmatize(x)) for x in tokens if x not in stopwords.words('english')]
 
+
 def contentTokenization(contenuto):
     tokens = nltk.word_tokenize(contenuto)
     tokens = removeStopwordsLemmatizeStemming(tokens)
     print(tokens) #indicizzare questa roba
 
-#schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT)
-#if not os.path.exists("indexdir"):
-#    os.mkdir("indexdir")
-#ix = create_in("indexdir", schema)
-#
-#writer = ix.writer()
 
-#QUI
-pagine = getParsedPage()
+#POPOLO L'INDICE LO SCHEMA
+schema = Schema(title=TEXT(stored=True), content=TEXT)
+
+#CREO L'INDICE
+if not os.path.exists("indexdir"):
+   os.mkdir("indexdir")
+ix = create_in("indexdir", schema)
+
+
+#POPOLO L'INDICE
+writer = ix.writer()
+pagine = xmlparserSAX.getParsedPage()
 for p in pagine:
-    contentTokenization(p.getContenuto())
-
-
-#for p in pagine:
-#    writer.add_document(title=p.getTitolo(), content=p.getContenuto(), path=u"https://en.wikipedia.org/wiki/"+p.getTitolo())
-#writer.commit()
-
+    #contentTokenization(p.getContenuto())
+    writer.add_document(title=p.getTitolo(), content=p.getContenuto())
+writer.commit()
+print('Fine creazione dell\'indice')
--- SearchingWhoosh.py
+++ SearchingWhoosh.py
@@ -1,4 +1,4 @@
-from whoosh.index import open_dir
+import whoosh.index as index
 from whoosh.fields import *
 from whoosh.qparser import QueryParser
 import os, os.path
@@ -6,37 +6,56 @@ import PySimpleGUI as sg #alternativa TKINTER
 
 # ix = open_dir("indexdir")
 # searcher = ix.searcher()
-        
-lista = list()
-tipo_ricerca='title'
-listbox = sg.Listbox(values=lista, size=(5000, 5000))
+#
+# lista = list()
+# tipo_ricerca='title'
+# listbox = sg.Listbox(values=lista, size=(5000, 5000))
+#
+# layout = [[sg.Text('Search engine JM', justification="center", size=(5000, 1))],
+#           [sg.Button("Cerca"), sg.InputText(size=(5000, 1))],
+#           [sg.Button('Ricerca nel titolo'), sg.Button('Ricerca nel contenuto')],
+#           [listbox]]
+#
+# # Create the Window
+# window = sg.Window('Search engine JM', layout)
+#
+# # Event Loop to process "events" and get the "values" of the inputs
+# while True:
+#     event, values = window.read()
+#     if event in (None, 'Cancel'):   # if user closes window or clicks cancel
+#         break
+#     # if event in 'Cerca':
+#     #     parser = QueryParser(tipo_ricerca, schema=ix.schema)
+#     #     query = parser.parse(values[0])
+#     #     results = searcher.search(query, terms=True)
+#     #     lista = list()
+#     #     if len(results) != 0:
+#     #         for x in results:
+#     #             lista.append(x['title'] + ': ' + x['path'] + f'   punteggio: {x.score}')
+#     #     listbox.Update(values=lista)
+#     # else:
+#     #     if event=='Ricerca nel titolo':
+#     #         tipo_ricerca='title'
+#     #     else:
+#     #         tipo_ricerca='content'
+#
+# window.close()
 
-layout = [[sg.Text('Search engine JM', justification="center", size=(5000, 1))],
-          [sg.Button("Cerca"), sg.InputText(size=(5000, 1))],
-          [sg.Button('Ricerca nel titolo'), sg.Button('Ricerca nel contenuto')],
-          [listbox]]
+#APERTURA INDICE
+ix = index.open_dir("indexdir")
 
-# Create the Window
-window = sg.Window('Search engine JM', layout)
+#CREO LA QUERY
+qc = QueryParser('content', schema=ix.schema) #QUERY SUL CONTENUTO
+qt = QueryParser('title', schema=ix.schema) #QUERY SUL TITOLO
+rc = qc.parse(u'Miller')
+rt = qt.parse(u'world')
 
-# Event Loop to process "events" and get the "values" of the inputs
-while True:
-    event, values = window.read()
-    if event in (None, 'Cancel'):   # if user closes window or clicks cancel
-        break
-    # if event in 'Cerca':
-    #     parser = QueryParser(tipo_ricerca, schema=ix.schema)
-    #     query = parser.parse(values[0])
-    #     results = searcher.search(query, terms=True)
-    #     lista = list()
-    #     if len(results) != 0:
-    #         for x in results:
-    #             lista.append(x['title'] + ': ' + x['path'] + f'   punteggio: {x.score}')
-    #     listbox.Update(values=lista)
-    # else:
-    #     if event=='Ricerca nel titolo':
-    #         tipo_ricerca='title'
-    #     else:
-    #         tipo_ricerca='content'
-
-window.close()
+with ix.searcher() as s:
+    resultsContent = s.search(rc, limit=None)
+    resultsTitle = s.search(rt, limit=None)
+    print('TITOLO')
+    for r in resultsTitle:
+        print(r)
+    print('\n\nCONTENUTO')
+    for r in resultsContent:
+        print(r)
