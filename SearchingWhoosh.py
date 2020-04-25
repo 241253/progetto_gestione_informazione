939301defa823f13ec5d0e18b382cf973659435e
michele <243582@studenti.unimore.it>
2020-04-25 18:36:05 +0200

    Z GUI

--- SearchingWhoosh.py
+++ SearchingWhoosh.py
@@ -2,47 +2,38 @@ import whoosh.index as index
 from whoosh.fields import *
 from whoosh.qparser import QueryParser
 import os, os.path
-import PySimpleGUI as sg #alternativa TKINTER
-
-# ix = open_dir("indexdir")
-# searcher = ix.searcher()
-#
-# lista = list()
-# tipo_ricerca='title'
-# listbox = sg.Listbox(values=lista, size=(5000, 5000))
-#
-# layout = [[sg.Text('Search engine JM', justification="center", size=(5000, 1))],
-#           [sg.Button("Cerca"), sg.InputText(size=(5000, 1))],
-#           [sg.Button('Ricerca nel titolo'), sg.Button('Ricerca nel contenuto')],
-#           [listbox]]
-#
-# # Create the Window
-# window = sg.Window('Search engine JM', layout)
-#
-# # Event Loop to process "events" and get the "values" of the inputs
-# while True:
-#     event, values = window.read()
-#     if event in (None, 'Cancel'):   # if user closes window or clicks cancel
-#         break
-#     # if event in 'Cerca':
-#     #     parser = QueryParser(tipo_ricerca, schema=ix.schema)
-#     #     query = parser.parse(values[0])
-#     #     results = searcher.search(query, terms=True)
-#     #     lista = list()
-#     #     if len(results) != 0:
-#     #         for x in results:
-#     #             lista.append(x['title'] + ': ' + x['path'] + f'   punteggio: {x.score}')
-#     #     listbox.Update(values=lista)
-#     # else:
-#     #     if event=='Ricerca nel titolo':
-#     #         tipo_ricerca='title'
-#     #     else:
-#     #         tipo_ricerca='content'
-#
-# window.close()
+from tkinter import *
+from tkinter import ttk
+from tkinter import scrolledtext
 
 #APERTURA INDICE
 ix = index.open_dir("indexdir")
+
+# GUI
+# Creazione finestra
+window = Tk()
+window.title("Il TOP SEARCHER di J e Z")
+
+# Creazione casella di testo
+txt = Entry(window,width=100)
+txt.grid(column=0, row=0)
+
+# Creazione bottone di ricerca
+btn = Button(window, text="Cerca")
+btn.grid(column=1, row=0)
+
+# Creazione bottoni di selezione di tipo ricerca
+btn = Button(window, text="Per titolo")
+btn.grid(column=0, row=1)
+btn = Button(window, text="Per contenuto")
+btn.grid(column=0, row=2)
+
+# Scrollview per visualizzare il risultato della ricerca
+scrollview = scrolledtext.ScrolledText(window,width=100,height=10)
+scrollview.grid(columnspan=2, row=3, sticky="nesw")
+
+# Loop finestra (mantiene la finestra aperta)
+window.mainloop()
 
 #CREO LA QUERY
 qc = QueryParser('content', schema=ix.schema) #QUERY SUL CONTENUTO
