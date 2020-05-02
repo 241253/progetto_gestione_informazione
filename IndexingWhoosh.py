2927152c054d040a850290e8b1fb0f66454e9eb8
241253 <241253@studenti.unimore.it>
2020-05-02 18:30:06 +0200

    J: TONGOLI (da una simpatica idea di Z)

--- IndexingWhoosh.py
+++ IndexingWhoosh.py
@@ -1,4 +1,6 @@
 import os
+import string
+
 from whoosh.fields import *
 from whoosh.index import create_in
 import xmlparserSAX
@@ -7,16 +9,21 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
 
-def removeStopwordsLemmatizeStemming(tokens):
+def tokensCleaner(tokens):
+    #processo di lemmatizzazione e stemmatizzazione
     wnl = nltk.WordNetLemmatizer()
     porter = PorterStemmer()
-    return [porter.stem(wnl.lemmatize(x)) for x in tokens if x not in stopwords.words('english')]
+    exclude = set(string.punctuation)
+    #rimozione stopwords e punteggiature
+    stopwordsToken = [porter.stem(wnl.lemmatize(x)) for x in tokens if x not in stopwords.words('english')]
+    return [x for x in stopwordsToken if x not in exclude]
+
 
 
 def contentTokenization(contenuto):
     tokens = nltk.word_tokenize(contenuto)
-    tokens = removeStopwordsLemmatizeStemming(tokens)
-    print(tokens) #indicizzare questa roba
+    tokens = tokensCleaner(tokens)
+    return tokens
 
 
 #POPOLO L'INDICE LO SCHEMA
@@ -29,10 +36,10 @@ ix = create_in("indexdir", schema)
 
 
 #POPOLO L'INDICE
-writer = ix.writer()
+#writer = ix.writer()
 pagine = xmlparserSAX.getParsedPage()
 for p in pagine:
-    #contentTokenization(p.getContenuto())
-    writer.add_document(title=p.getTitolo(), content=p.getContenuto())
-writer.commit()
+    contentTokenization(p.getContenuto())
+    #writer.add_document(title=p.getTitolo(), content=p.getContenuto())
+#writer.commit()
 print('Fine creazione dell\'indice')
