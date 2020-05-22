20b312d44a0ec0246a841e23ba07476adbad6f6b
241253 <241253@studenti.unimore.it>
2020-05-22 12:17:16 +0200

    J: wiki - 1000 pagine

--- file_writer.py
+++ file_writer.py
@@ -1,11 +1,11 @@
 with open('./wiki.xml', 'r', encoding='utf-8') as file:
     data = file.readlines()
     count = 0
-    with open('wiki.xml', 'a+', encoding='utf-8') as file2:
+    with open('test_wiki.xml', 'a+', encoding='utf-8') as file2:
         for l in data:
             if '</page>' in l:
                 count +=1
             file2.write(l)
-            if count == 10:
+            if count == 1000:
                 file2.write('</mediawiki>')
                 break
