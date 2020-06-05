59b292b79b0318c17c39f1a9ca09a864668ef6a5
michele <243582@studenti.unimore.it>
2020-06-05 17:12:38 +0200

    Z: google search

--- google_search.py
+++ google_search.py
@@ -1,5 +1,5 @@
 from googlesearch import search
-
+import time
 
 def google_search(query):
     my_results_list = []
@@ -7,12 +7,24 @@ def google_search(query):
                     lang = 'en',  # The language
                     num = 10,     # Number of results per page
                     start = 0,    # First result to retrieve
-                    stop = 40,    # Last result to retrieve
+                    stop = 29,    # Last result to retrieve
                     pause = 2.0,  # Lapse between HTTP requests
                    ):
         my_results_list.append(i)
-        print(i)
     return my_results_list
 
-if __name__ == '__main__':
-    google_search('DNA site:en.wikipedia.org')
+queries = ["DNA"]
+# queries = ["DNA"], "Apple", "Epigenetics", "Hollywood", "Maya", "Microsoft", "Precision", "Tuscany",
+#            "99 balloons", "Computer Programming", "Financial meltdown", "Justin Timberlake",
+#            "Least Squares", "Mars robots", "Page six", "Roman Empire", "Solar energy",
+#            "Statistical Significance", "Steve Jobs", "The Maya", "Triple Cross", "US Constitution",
+#            "Eye of Horus", "Madam I'm Adam", "Mean Average Precision", "Physics Nobel Prizes",
+#            "Read the manual", "Spanish Civil War", "Do geese see god", "Much ado about nothing"]
+
+l = []
+for i in queries:
+    l += google_search(i + " site:en.wikipedia.org")
+    print(i)
+    time.sleep(3)
+
+print(l)
