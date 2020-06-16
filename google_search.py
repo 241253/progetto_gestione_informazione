1d6401374b2075d16a15ba941bbbb60e2dd1f407
241253 <241253@studenti.unimore.it>
2020-06-16 10:50:00 +0200

    J: google_search selenium version

--- google_search.py
+++ google_search.py
@@ -8,9 +8,9 @@ def google_search(query):
     my_results_list = []
     for i in search(query,  # The top level domain
                     lang = 'en',  # The language
-                    num = 40,     # Number of results per page
+                    num = 10,     # Number of results per page
                     start = 0,    # First result to retrieve
-                    stop = 40,    # Last result to retrieve
+                    stop = 30,    # Last result to retrieve
                     pause = 2.0,  # Lapse between HTTP requests
                    ):
         my_results_list.append(i)
@@ -32,34 +32,31 @@ def ricerca(i):
             file.write(x+'\n')
 
 
-# def getTitle(s):
-#     l = s.replace('\n', '').split(':')
-#     temp = ''
-#     for x in range(2,len(l)):
-#         temp += l[x] + ('' if x == len(l) else ':')
-#     return temp[:-1]
-#
-# def correctFormat(s):
-#     s=s.replace('_', ' ')
-#     s=s.replace('\n', '')
-#     return s
-#
+def getTitle(s):
+    l = s.replace('\n', '').split(':')
+    temp = ''
+    for x in range(2,len(l)):
+        temp += l[x] + ('' if x == len(l) else ':')
+    return temp[:-1]
+
+def correctFormat(s):
+    s=s.replace('_', ' ')
+    s=s.replace('\n', '')
+    return s
+
 if __name__ == '__main__':
-    ricerca(21)
-#     file = open('ricerca.txt', 'r')
-#     pages = []
-#     with file:
-#         for x in file:
-#             pages.append(x)
-#     pages = [correctFormat(unquote(i[30::])) for i in pages]
-#
-#     id = []
-#     with open('enwiki-20200520-pages-articles-multistream-index.txt', 'r', encoding='utf-8') as file:
-#         lines = file.readlines()
-#         print(len(lines))
-#         for line in lines:
-#             if getTitle(line) in pages:
-#                 print(getTitle(line),' --> ', line.split(':')[1])
-#                 id.append(line.split(':')[1])
-#     print('LUNGHEZZA: ', len(id))
-#     print(id)
+    file = open('ricerca.txt', 'r')
+    pages = []
+    with file:
+        for x in file:
+            pages.append(x)
+    pages = [correctFormat(unquote(i[30::])) for i in pages]
+
+    id = []
+    with open('enwiki-20200520-pages-articles-multistream-index.txt', 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+        for line in lines:
+            if getTitle(line) in pages:
+                print(getTitle(line),' --> ', line.split(':')[1])
+                id.append(line.split(':')[1])
+    print('LUNGHEZZA: ', len(id))
