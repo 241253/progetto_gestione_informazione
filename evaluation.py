023b1ea0abe4cc98a2ab6ff4c080aa36d60bb647
241253 <241253@studenti.unimore.it>
2020-06-23 18:51:16 +0200

    J: evaluation

--- evaluation.py
+++ evaluation.py
@@ -1,15 +1,11 @@
+from math import log
+
 import numpy as np
 from sklearn.metrics import average_precision_score, ndcg_score
 
-# # EVALUATION
-# # we have groud-truth relevance of some answers to a query:
-#
-# # we predict some scores (relevance) for the answers
-# print(res)
 from whoosh import scoring, index
 from whoosh.qparser import MultifieldParser
 from whoosh.scoring import MultiWeighting, TF_IDF, Frequency
-from SearchingWhoosh import txt
 
 queries = ["DNA", "Apple", "Epigenetics", "Hollywood", "Maya", "Microsoft", "Precision", "Tuscany",
 "99 balloons", "Computer Programming", "Financial meltdown", "Justin Timberlake",
@@ -17,11 +13,6 @@ queries = ["DNA", "Apple", "Epigenetics", "Hollywood", "Maya", "Microsoft", "Pre
 "Statistical Significance", "Steve Jobs", "The Maya", "Triple Cross", "US Constitution",
 "Eye of Horus", "Madam I'm Adam", "Mean Average Precision", "Physics Nobel Prizes",
 "Read the manual", "Spanish Civil War", "Do geese see god", "Much ado about nothing"]
-
-ix = index.open_dir("indexdir/index")
-wBM25 = scoring.BM25F(B=0.75, title_B=2.0, body_B=1.0, category_B=1.0, infobox_B=1.0, K1=1.5)
-mw = MultiWeighting(TF_IDF(), id=wBM25, keys=Frequency())
-
 
 def get_results(search_key):
     q = MultifieldParser(['title', 'body', 'category', 'infobox'], schema=ix.schema)
@@ -34,24 +25,72 @@ def get_results(search_key):
             if r['title'][:9] != 'Category:':
                 l.append(r['title'])
                 count += 1
+            else:
+                print(r['title'])
             if count == 10:
                 break
     return l
 
-true_relevance = np.asarray([[6, 5, 4, 3, 2, 1, 1, 1, 1, 1]])
-for q in queries:
-    ground_truth = []
-    with open('evaluation_files/'+q, 'r') as file:
-        ground_truth = file.readlines()
-    risultati = get_results(q)
-    temp = []
-    point = 6
-    for r in risultati:
-        if r in ground_truth:
-            temp.append(point if point > 0 else 1)
-        else:
-            temp.append(0)
-        point -= 1
-    scores = np.asarray([temp])
-    res = ndcg_score(true_relevance, scores)
-    print(q, ": ", res)
+def sklearn_ndcg_evaluation():
+    true_relevance = np.asarray([[6, 5, 4, 3, 2, 1, 1, 1, 1, 1]])
+    media = 0
+    for q in queries:
+        ground_truth = []
+        with open('evaluation_files/'+q, 'r') as file:
+            ground_truth = file.readlines()
+        risultati = get_results(q)
+        temp = []
+        point = 6
+        for r in risultati:
+            if r in ground_truth:
+                temp.append(point if point > 0 else 1)
+            else:
+                temp.append(0)
+            point -= 1
+        scores = np.asarray([temp])
+        res = ndcg_score(true_relevance, scores)
+        media += res
+        print(q, ": ", res, scores)
+    print('MEDIA: ', media/30)
+
+def manual_ndcg_evaluation():
+    true_relevance = [6, 5, 4, 3, 2, 1, 1, 1, 1, 1]
+    idcg = 6
+    for i in range(2, len(true_relevance)):
+        idcg += true_relevance[i] / log(i, 2)
+    print(idcg)
+    media = 0
+    for q in queries:
+        ground_truth = []
+        with open('evaluation_files/'+q, 'r') as file:
+            ground_truth = file.readlines()
+        ground_truth = [x[:-1] for x in ground_truth]
+        risultati = get_results(q)
+        scores = []
+        point = 6
+        # print(risultati)
+        # print(ground_truth)
+        for r in risultati:
+            if r in ground_truth:
+                scores.append(point if point > 0 else 1)
+            else:
+                scores.append(0)
+            point -= 1
+        res = scores[0]
+        for i in range(2,len(scores)):
+            res += scores[i]/log(i, 2)
+        res =res / idcg
+        media += res
+        print(q, ": ", res, scores)
+    print('MEDIA: ', media/30)
+
+if __name__ == '__main__':
+    ix = index.open_dir("indexdir/index")
+    wBM25 = scoring.BM25F(B=0.75, title_B=2.0, body_B=1.0, category_B=1.0, infobox_B=1.0, K1=1.5)
+    mw = MultiWeighting(TF_IDF(), id=wBM25, keys=Frequency())
+
+    # print('Risultati con SKLEARN:')
+    # sklearn_ndcg_evaluation()
+
+    print('\nRisultati con ROBA MANUALE:')
+    manual_ndcg_evaluation()
