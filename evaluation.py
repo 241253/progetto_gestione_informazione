97361d1c6bea8a3da3457c927b6b13bd7981f513
michele <243582@studenti.unimore.it>
2020-06-29 11:44:30 +0200

    Z: primo risultato di evaluation fatto (creazione cartella evaluation_results)

--- evaluation.py
+++ evaluation.py
@@ -14,11 +14,11 @@ queries = ["DNA", "Apple", "Epigenetics", "Hollywood", "Maya", "Microsoft", "Pre
 "Eye of Horus", "Madam I'm Adam", "Mean Average Precision", "Physics Nobel Prizes",
 "Read the manual", "Spanish Civil War", "Do geese see god", "Much ado about nothing"]
 
-def get_results(search_key):
+def get_results(search_key, weighting):
     q = MultifieldParser(['title', 'body', 'category', 'infobox'], schema=ix.schema)
     r = q.parse(search_key)
     l = []
-    with ix.searcher(weighting=mw) as searcher:
+    with ix.searcher(weighting=weighting) as searcher:
         results = searcher.search(r, limit=20)
         count = 0
         for r in results:
@@ -51,19 +51,18 @@ def get_results(search_key):
 #         print(q, ": ", res, scores)
 #     print('MEDIA: ', media/30)
 
-def ndcg_evaluation():
+def ndcg_evaluation(weighting):
     true_relevance = [6, 5, 4, 3, 2, 1, 1, 1, 1, 1]
     idcg = 6
     for i in range(2, len(true_relevance)):
         idcg += true_relevance[i] / log(i, 2)
-    print('idcg value: ', idcg)
     media = 0
     for q in queries:
         ground_truth = []
         with open('evaluation_files/'+q, 'r') as file:
             ground_truth = file.readlines()
         ground_truth = [x[:-1] for x in ground_truth]
-        risultati = get_results(q)
+        risultati = get_results(q, weighting)
         scores = []
         point = 6
         # print(risultati)
@@ -79,20 +78,20 @@ def ndcg_evaluation():
             res += scores[i]/log(i+1, 2)
         res =res / idcg
         media += res
-        print(q, ": ", res, scores)
+        # print(q, ": ", res, scores)
     print('MEDIA: ', media/30)
 
-def map_evaluation():
+def map_evaluation(weighting):
+    sumMeanAveragePrecision = 0.0
     for q in queries:
         ground_truth = []
         with open('evaluation_files/'+q, 'r') as file:
             ground_truth = file.readlines()
         ground_truth = [x[:-1] for x in ground_truth]
-        risultati = get_results(q)
+        risultati = get_results(q, weighting)
         precision = []
         countRilevanti = 0
         sumAveragePrecision = 0.0
-        sumMeanAveragePrecision = 0.0
         for i in range(len(risultati)):
             if risultati[i] in ground_truth:
                 countRilevanti += 1
@@ -100,21 +99,25 @@ def map_evaluation():
             precision.append(countRilevanti / (i+1))
 
         sumMeanAveragePrecision += sumAveragePrecision / len(ground_truth)
-        print(q, ":", precision)
+        # print(q, ":", sumAveragePrecision / len(ground_truth), precision)
 
-    print('MEDIA: ', sumMeanAveragePrecision / 30)
-    return sumMeanAveragePrecision / 30
+    print('MEDIA: ', sumMeanAveragePrecision / 30 )
 
 if __name__ == '__main__':
     ix = index.open_dir("indexdir/index")
     wBM25 = scoring.BM25F(B=0.75, title_B=2.0, body_B=1.0, category_B=1.0, infobox_B=1.0, K1=1.5)
-    mw = MultiWeighting(wBM25, id=wBM25, keys=Frequency())
+    wtf = TF_IDF()
 
     # COMMENTO
 
-    print("NDCG EVALUATION:")
-    ndcg_evaluation()
+    print("NDCG EVALUATION con BM25:")
+    ndcg_evaluation(wBM25)
+    print("NDCG EVALUATION con TF_IDF:")
+    ndcg_evaluation(wtf)
 
-    print("\nMAP EVALUATION")
-    map_evaluation()
+    print("\n\n")
 
+    print("MAP EVALUATION con BM25")
+    map_evaluation(wBM25)
+    print("MAP EVALUATION con TF_IDF")
+    map_evaluation(wtf)
